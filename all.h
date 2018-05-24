d409c0414f75024d3832ffabdae3d380ce986cf6
Jules Rangel <epicjules@gmail.com>
2018-05-24 00:48:35 -0500

    Dfode at least working!

--- all.h
+++ all.h
@@ -118,23 +118,50 @@ struct itikBanksJacobian
         a31 = parameters[6];
         d3 = parameters[7];
     }
-	// Row order 
+	//Equation (7) Wei 2014
+	// Row ordr
 	// a b
 	// c d 
 	// is returned as  [a b c d ] ^ T
+
+	//
+	//{ {D1, D2, D3},    { { x1, x2, x3 },
+	//{ D4, D5, D6 }, X  { x4, x5, x6 },        
+	//{ D7, D8, D9 }}    { x7, x8, x9 }}
+	//=
+	//(D1 x1 + D2 x4 + D3 x7 | D1 x2 + D2 x5 + D3 x8 | D1 x3 + D2 x6 + D3 x9
+	//	D4 x1 + D5 x4 + D6 x7 | D4 x2 + D5 x5 + D6 x8 | D4 x3 + D5 x6 + D6 x9
+	//	D7 x1 + D8 x4 + D9 x7 | D7 x2 + D8 x5 + D9 x8 | D7 x3 + D8 x6 + D9 x9)
     void operator()(const stateType &x, stateType &dxdt, double t)
     {
+		dxdt[0] = (1 - E(t) * a13 - H(t) * a12 - 2 * T(t)) * x[0] + (-T(t) * a12)* x[3] + (-T(t) * a13)* x[6];
+		dxdt[1] = (1 - E(t) * a13 - H(t) * a12 - 2 * T(t)) * x[1] + (-T(t) * a12)* x[4] + (-T(t) * a13)* x[7];
+		dxdt[2] = (1 - E(t) * a13 - H(t) * a12 - 2 * T(t)) * x[2] + (-T(t) * a12)* x[5] + (-T(t) * a13)* x[8];
 
-        dxdt[0] = 1 - E(t) * a13 - H(t) * a12 - 2 * T(t);
-        dxdt[1] = -T(t) * a12;
-        dxdt[2] = -T(t) * a13;
-        dxdt[3] = -H(t) * a21;
-        dxdt[4] = -T(t) * a21 - H(t) * r2 - r2 * (H(t) - 1);
-        dxdt[5] = 0;
-        dxdt[6] = (E(t) * r3) / (T(t) + k3) - E(t) * a31 - (E(t) * T(t) * r3) / pow((T(t) + k3), 2);
-        dxdt[7] = 0;
-        dxdt[8] = (T(t) * r3) / (T(t) + k3) - T(t) * a31 - d3;
+		dxdt[3] = (-H(t) * a21) * x[0] + (-T(t) * a21 - H(t) * r2 - r2 * (H(t) - 1)) * x[3];
+		dxdt[4] = (-H(t) * a21) * x[1] + (-T(t) * a21 - H(t) * r2 - r2 * (H(t) - 1)) * x[4];
+		dxdt[5] = (-H(t) * a21) * x[2] + (-T(t) * a21 - H(t) * r2 - r2 * (H(t) - 1)) * x[5];
+	
+		dxdt[6] = ((E(t) * r3) / (T(t) + k3) - E(t) * a31 - (E(t) * T(t) * r3) / pow((T(t) + k3), 2))* x[0] + ((T(t) * r3) / (T(t) + k3) - T(t) * a31 - d3)* x[6];
+		dxdt[7] = ((E(t) * r3) / (T(t) + k3) - E(t) * a31 - (E(t) * T(t) * r3) / pow((T(t) + k3), 2))* x[1] + ((T(t) * r3) / (T(t) + k3) - T(t) * a31 - d3)* x[7];
+		dxdt[8] = ((E(t) * r3) / (T(t) + k3) - E(t) * a31 - (E(t) * T(t) * r3) / pow((T(t) + k3), 2))* x[2] + ((T(t) * r3) / (T(t) + k3) - T(t) * a31 - d3)* x[8];
     }
+
+	//The jacobian
+	//matrix<double> Dfu(double t) {
+	//	matrix<double> M(STATE_SIZE, STATE_SIZE);
+	//	M(0, 0) = 1 - E(t) * a13 - H(t) * a12 - 2 * T(t);
+	//	M(0, 1) = -T(t) * a12;
+	//	M(0, 2) = -T(t) * a13;
+
+	//	M(1, 0) = -H(t) * a21;
+	//	M(1, 1) = -T(t) * a21 - H(t) * r2 - r2 * (H(t) - 1);
+	//	M(1, 2) = 0;
+
+	//	M(2, 0) = (E(t) * r3) / (T(t) + k3) - E(t) * a31 - (E(t) * T(t) * r3) / pow((T(t) + k3), 2);
+	//	M(2, 1) = 0;
+	//	M(2, 2) = (T(t) * r3) / (T(t) + k3) - T(t) * a31 - d3;
+	//}
 };
 
 //Gets the jacobian following the procedure in eq (6)&(7) of the
