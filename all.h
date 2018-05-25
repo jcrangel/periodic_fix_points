c136cced0425fcf797230b3854a220f9ce45312c
Jules Rangel <epicjules@gmail.com>
2018-05-25 10:43:35 -0500

    newton poincare working

--- all.h
+++ all.h
@@ -3,14 +3,16 @@
 
 #include <iostream>
 #include <cmath>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/io.hpp>
+//Boosst lib
+//#include <boost/numeric/ublas/matrix.hpp>
+//#include <boost/numeric/ublas/io.hpp>
+#include <Eigen/Dense>
 #include <boost/numeric/odeint.hpp>
 #include <boost/math/interpolators/barycentric_rational.hpp>
 
 #define STATE_SIZE 3
 
-using namespace boost::numeric::ublas;
+using namespace Eigen;
 using namespace boost::numeric::odeint;
 
 //An array [x1(t),x2(t),x3(t)]	
@@ -76,10 +78,10 @@ struct itikBanks
 
 //Reshape a vector Size N^2 into a Matrix NxN, 
 //vector is assumed to come in row order
-matrix<double> reshapeVectorToMatrix(const stateType x) {
+Matrix3d reshapeVectorToMatrix(const stateType x) {
 
 	int N = sqrt(x.size());
-	matrix<double> A(N,N);
+	Matrix3d A(N,N);
 	
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
@@ -167,7 +169,7 @@ struct itikBanksJacobian
 //Gets the jacobian following the procedure in eq (6)&(7) of the
 //paper Wei 2014
 template <class T>
-matrix<double> DFode(T &fun,stateType initialCondition,double tau,double d)
+Matrix3d DFode(T &fun,stateType initialCondition,double tau,double d)
 {
 // xp=[0, tau];
 	stateType xp{0,tau};
@@ -193,24 +195,113 @@ matrix<double> DFode(T &fun,stateType initialCondition,double tau,double d)
     //  }
     std::vector<stateType> V;
     std::vector<double> t;
-
+	//!!!! this param shoul exist only on one place not to be repated
     std::vector<double> param = { 1,2.5,0.5,1.5,4.5,1,0.2,0.5 };
     std::vector<double> identityMatrixVector = {1,0,0,0,1,0,0,0,1};
     itikBanksJacobian Dfu(param,state,tt);
     steps = integrate_adaptive(make_controlled<error_stepper_type>(1.0e-10, 1.0e-6), 
             Dfu, identityMatrixVector, xp[0], xp[1], 0.001,
             push_back_state_and_time(V, t));
-    for( size_t i=0; i <= steps; i++ )
-	{
-        std::cout << V[i][0] << '\t' << V[i][1] << '\t'
-                  << V[i][2] << '\t' << V[i][3] << '\t' << V[i][4] << '\t'
-                  << V[i][5] << '\t' << V[i][6] << '\t' << V[i][7] << '\t'
-                  << V[i][8] << '\n';
-    }
+ //   for( size_t i=0; i <= steps; i++ )
+	//{
+ //       std::cout << V[i][0] << '\t' << V[i][1] << '\t'
+ //                 << V[i][2] << '\t' << V[i][3] << '\t' << V[i][4] << '\t'
+ //                 << V[i][5] << '\t' << V[i][6] << '\t' << V[i][7] << '\t'
+ //                 << V[i][8] << '\n';
+ //   }
 
 	// Returns the last matrix since 
 	// DF(x) = v(tau).See page 12 research notebook
 	return reshapeVectorToMatrix(V.back());
+};
+
+class fixPoint {
+public:
+	bool convergent;
+	bool stability;
+	Vector3d solution;
+
+	fixPoint(bool convergent_ ,bool stability_,Vector3d solution_) :
+		convergent(convergent_),
+		stability(stability_),
+		solution(solution_) {}
+
+};
+//Calculate all eigenvalues of matrix A, if all of them are less tahn 1
+// the matrix is stable()true.
+bool isStable(MatrixXd A) {
+	VectorXcd eivals = A.eigenvalues();
+	for (int i = 0; i < eivals.size(); ++i)
+		if (std::abs(eivals[i]) >= 1)
+			return false;
+
+	return true;
+};
+
+template <class T>
+fixPoint newtonPoincare(T &fun, stateType initialCondition, double tau, double d) {
+	int n = 0;            //%initialize iteration counter
+	double eps = 1;          //%initialize error
+	std::vector<double> xp{ 0, tau }; //% define the span of the computational domain
+	double tol = 1e-6;
+	int maxStep = 100;
+
+	Vector3d Xk(initialCondition.data());
+	Vector3d Pxk;
+	Matrix3d df;
+	std::vector<stateType> u;
+	std::vector<double> t;
+	Matrix3d A;
+	Matrix3d I = Matrix3d::Identity();
+	Vector3d y;
+	Vector3d Xplus;
+	size_t steps;
+	typedef runge_kutta_cash_karp54<stateType> error_stepper_type;
+
+	//stateType initCond;
+
+	while (n < maxStep) {					//!@_@
+		df = DFode(fun, std::vector<double> {Xk[0], Xk[1], Xk[2]}, tau, d);  //Jacobian
+		initialCondition[0] = Xk[0];
+		initialCondition[1] = Xk[1];
+		initialCondition[2] = Xk[2]+d;
+
+	    steps = integrate_adaptive(make_controlled<error_stepper_type>(1.0e-10, 1.0e-6),
+			fun,				//!@_@							
+			initialCondition,
+			xp[0], xp[1], 0.001, push_back_state_and_time(u, t));
+		//[t, u] = ode15s(functionName, xp, [Xk(1), Xk(2), Xk(3) + d], [], [parameters d]);
+		
+		//!@This should be done in a bette wway
+		Pxk[0] = u.back()[0];
+		Pxk[1] = u.back()[1];
+		Pxk[2] = u.back()[2];
+
+		A = df - I;
+		//Solve Ay = (Pxk-Xk)
+		y = A.colPivHouseholderQr().solve(Pxk-Xk);
+		Xplus = Xk - y;
+		
+		eps = y.norm();
+		if (eps < tol)
+			break;
+	
+		Xk = Xplus;  //%update x
+		n++;
+		
+	}
+	// 0 means unstable
+	double stability = 0;
+	bool convergence = false;
+	if (n < maxStep){
+		convergence = true;
+		
+		if (isStable(df))
+			stability = 1;
+
+	}
+	fixPoint ss(convergence, stability, Xk);
+	return ss;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/*
+To do:
+1. Define StateTye as Vector3d so is congruent with all the program.
+*/
+
 #include "all.h"
 
 
@@ -6,7 +11,12 @@ int main(){
 	stateType IC = {0,1,0.06};
 	std::vector<double> param={1,2.5,0.5,1.5,4.5,1,0.2,0.5};
 	itikBanks fun(param);
-    matrix<double> m = DFode(fun,IC,10,0.5);
-    std::cout<< m <<std::endl;
+	Matrix3d m = DFode(fun,IC,10,0.5);
+	std::cout << m << std::endl;
+
+	fixPoint a = newtonPoincare(fun, IC, 10, 0.5);
+
+	std::cout <<"Convergent?:\n"<< a.convergent <<"\n Sol: \n"<< a.solution <<"\n Stable?:\n" << a.stability<<std::endl;
 	std::cin.get();//VS windows only
-	}
+	return 0;
+}
